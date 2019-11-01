320a198f52b5fcad3a7e00d244492b783cad59f4
MuggleLee <770796059@qq.com>
2019-11-01 17:57:42 +0800

    add

---
+++ src/main/java/com/dmt/importexcel/Listener/DemoDataListener.java
@@ -0,0 +1,44 @@
+package com.dmt.importexcel.Listener;
+
+import com.dmt.importexcel.Model.DemoData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Muggle Lee
+ * @Date: 2019/11/1 17:46
+ */
+// 有个很重要的点 DemoDataListener 不能被spring管理，要每次读取excel都要new,然后里面用到spring可以构造方法传进去
+public class DemoDataListener extends AnalysisEventListener<DemoData> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DemoDataListener.class);
+    /**
+     * 每隔5条存储数据库，实际使用中可以3000条，然后清理list ，方便内存回收
+     */
+    private static final int BATCH_COUNT = 5;
+    List<DemoData> list = new ArrayList<DemoData>();
+
+    @Override
+    public void invoke(DemoData data, AnalysisContext context) {
+        LOGGER.info("解析到一条数据:{}", JSON.toJSONString(data));
+        list.add(data);
+        if (list.size() >= BATCH_COUNT) {
+            saveData();
+            list.clear();
+        }
+    }
+
+    @Override
+    public void doAfterAllAnalysed(AnalysisContext context) {
+        saveData();
+        LOGGER.info("所有数据解析完成！");
+    }
+
+    /**
+     * 加上存储数据库
+     */
+    private void saveData() {
+        LOGGER.info("{}条数据，开始存储数据库！", list.size());
+        LOGGER.info("存储数据库成功！");
+    }
+}
---
+++ src/main/java/com/dmt/importexcel/Model/DemoData.java
@@ -0,0 +1,14 @@
+package com.dmt.importexcel.Model;
+
+import java.util.Date;
+
+/**
+ * @author Muggle Lee
+ * @Date: 2019/11/1 17:46
+ */
+@Data
+public class DemoData {
+    private String string;
+    private Date date;
+    private Double doubleData;
+}
